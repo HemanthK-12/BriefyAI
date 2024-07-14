d28f70ca6da04637318a49cadd5599e8568f2c6e
HemanthK-12 <hemanthkarthikeya12@gmail.com>
2024-07-14 17:48:54 +0530

    Update project

--- script.js
+++ script.js
@@ -1,57 +1,24 @@
-const textArea = document.getElementById("text_to_summarize");
-const submitButton = document.getElementById("submit-button");
-const summarizedTextArea = document.getElementById("summary");
+const Api = () => {
+  let text = document.getElementById("text_to_summarize").value;
+  let output = document.getElementById("summary");
 
-submitButton.disabled = true;
-
-textArea.addEventListener("input", verifyTextLength);
-submitButton.addEventListener("click", submitData);
-
-function verifyTextLength(e) {
-  const textarea = e.target;
-
-  if (textarea.value.length > 200 && textarea.value.length < 100000) {
-    submitButton.disabled = false;
-  } else {
-    submitButton.disabled = true;
-  }
-}
-
-function submitData(e) {
-
-  submitButton.classList.add("submit-button--loading");
-
-  const text_to_summarize = textArea.value;
-
-  var myHeaders = new Headers();
-  myHeaders.append("Content-Type", "application/json");
-
-  var raw = JSON.stringify({
-    "text_to_summarize": text_to_summarize
-  });
-
-  var requestOptions = {
-    method: 'POST',
-    headers: myHeaders,
-    body: raw,
-    redirect: 'follow'
+  async function query(data) {
+    const response = await fetch(
+      "https://api-inference.huggingface.co/models/facebook/bart-large-cnn",
+      {
+        headers: {
+          Authorization: "Bearer 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",
+          "Content-Type": "application/json",
+        },
+        method: "POST",
+        body: JSON.stringify(data),
+      }
+    );
+    const result = await response.json();
+    return result;
   };
 
-  // Send the text to the server using fetch API
-
-  // Note - here we can omit the “baseUrl” we needed in Postman and just use a relative path to “/summarize” because we will be calling the API from our Replit!  
-  fetch('/summarize', requestOptions)
-    .then(response => response.text()) // Response will be summarized text
-    .then(summary => {
-      // Do something with the summary response from the back end API!
-
-      // Update the output text area with new summary
-      summarizedTextArea.value = summary;
-
-      // Stop the spinning loading animation
-      submitButton.classList.remove("submit-button--loading");
-    })
-    .catch(error => {
-      console.log(error.message);
-    });
-}
+  query({ "inputs": text, }).then((response) => {
+    output.innerHTML = response[0]?.summary_text;
+  });
+};
